37a80a3cd30fb630badd80698817c78e53247a01
MeletChirino <melet.chirino@gmail.com>
2021-09-24 16:00:38 +0200

    [UPDATED] Test file

--- UseCase.py
+++ UseCase.py
@@ -1,35 +1,59 @@
 from crysuml.UseCase import *
 
-client = Actor(name = 'client',)
+operator = Actor(name = 'operator',)
 database = Actor(name='Database', type='database')
-cloud = Actor(name="Google", type='cloud')
-read = Case(
-        name='Read',
-        note="jas;ldkfjh;ladksj",
-        links=[
-            {"actor": client, "type": "simple",},
-            {"actor": database, "type": "simple",},
-            ]
-        )
-write = Case(
-        name='Mentir',
-        note="jas;ldkfjh;ladksj",
-        links=[
-            {"actor": client, "type": "simple",},
-            {"case": read, "type": "include",},
-            {"actor": cloud, "type": "simple",},
-            ]
-        )
-delete = Case(
-        name = "Delete",
+robot = Actor(name="Robot", type='component')
+configure = Case(
+        name = 'Configure',
         links = [
-            {"actor": database, "type": "simple"},
-            {"actor": client, "type": "simple"},
-            {"case": write, "type": "extends"},
+            link(actor=operator, type='simple'),
+            link(exigence="configure"),
+            link(actor=robot, type='simple'),
             ]
         )
+select_use = Case(
+        name = 'Select Use',
+        description = '''
+        User can select mode that robot will behave
+        ''',
+        links = [
+            link(actor=operator, type='simple')
+            ]
+        )
+move = Case(
+        name = 'Move Around the house',
+        description = '''
+        Robot will move around the hous avoiding obstacles
+        ''',
+        links = [
+            link(exigence='move'),
+            link(case=select_use, type='extends'),
+            link(actor=robot, type='simple'),
+            ]
+        )
+eat = Case(
+        name = 'Eat some food',
+        description = '''
+        The Robot must eat only the amount of food given
+        ''',
+        links = [
+            link(exigence='eat'),
+            link(case=select_use, type='extends'),
+            link(actor=robot, type='simple'),
+            ]
+        )
+map_house = Case(
+        name = 'Map all the house',
+        description = 'Robot must save a map of the whole place',
+        links = [
+            link(case=move, type='extends'),
+            link(actor=database, type='simple'),
+            link(exigence='map')
+            ]
+        )
+cases_list = [select_use, move, eat, map_house]
 
 use_case_diagram = Diagram(
-        cases = [read, write, delete],
-        actors = [client, database, cloud]
+        cases = cases_list,
+        actors = [operator, database, robot]
         )
