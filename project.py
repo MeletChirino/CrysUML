7d26fa436f3eb77f5cf4f5d57f2c35e4cfacc006
Melet David <melet.chirino@gmail.com>
2021-09-30 16:33:40 +0200

    [UPDATE] Test Files

--- project.py
+++ project.py
@@ -9,13 +9,19 @@ def main():
             name = "Exigences",
             description = "This file shows exigences",
             instance_list = exigences_list,
-            footer = "I like that girl"
+            footer = "We all agreed 28/sept/2021"
             )
     create_md(
             name = "Cases",
             description = "This file shows cases",
             instance_list = cases_list,
-            footer = "I like that girl, shall do el delicioso?"
+            footer = "We all agreed 28/sept/2021"
+            )
+    create_md(
+            name = "Actors",
+            description = "This are the actors of our systems",
+            instance_list = actors_list,
+            footer = "Those actors are going to do the best for our project"
             )
     #matrix(cases_list, exigences_list, 'exigence')
     return 0
