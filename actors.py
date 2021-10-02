1fb4eafe7f9de71ad518a25e407ccca2a16fce7c
Melet David <melet.chirino@gmail.com>
2021-10-02 10:45:17 +0200

    [UPDATE] test files

--- actors.py
+++ actors.py
@@ -1,4 +1,12 @@
 from crysuml.UseCase import Actor
 
-operator = Actor(name = 'Operator',)
-raspi = Actor(name='Unite_Centrale', type='component')
+operator = Actor(
+        name = 'Operator',
+        description = 'Operator is going to say the robot what they should do'
+        )
+raspi = Actor(
+        name='Unite_Centrale',
+        verbose_name='Unite centrale',
+        description = 'This is going to control the whole stuff',
+        type='component'
+        )
--- exigences.py
+++ exigences.py
@@ -3,22 +3,27 @@ from crysuml.exigences import exigence
 exigences_list = [
         exigence(
             name = "wireless",
+            verbose_name = "Comunication sans fils",
             description = "L'interface devra se comuniquer avec le robot par une signal sans-fil."
             ),
         exigence(
             name = "code_language",
+            verbose_name = "Langage de programation",
             description = "L'interface devra etre codee en C ou C++."
             ),
         exigence(
             name = "messages",
-            description = "L'interface devra envoyer des messages operationels vers le robot."
+            verbose_name = "Commander le robot",
+            description = "L'interface devra commander le robot a distance."
             ),
         exigence(
             name = "real_time",
+            verbose_name = "Temps reel",
             description = 'L\'interface doit montrer l\'image en temps reel du robot'
             ),
         exigence(
             name = "final_report",
+            verbose_name = "Montrer l'info",
             description = 'L\'interface doit montrer l\'information acquise par le robot'
             ),
         ]
