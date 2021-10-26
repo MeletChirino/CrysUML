2463e6075058cdf0eede7494201247a28b8464e1
Melet David <melet.chirino@gmail.com>
2021-10-26 16:42:29 +0200

    [UPDATE] Test Files

--- components.py
+++ components.py
@@ -1,14 +1,29 @@
 """Components file"""
 from crysuml.PhysicArch import *
 
-communication_api = Component(
-        name = "API",
+pc_api = Component(
+        name = "PC_API",
         description = "API pour etablir la communication entre le robot et l'interface graphique du PC",
         categorie = "communication",
         ports = (
             'socket_tcp',
             'lidar_data_send',
             'params',
+            'robot_mecanics',
+            'recv_position_data',
+            'send_position_data',
+            )
+        )
+robot_api = Component(
+        name = 'robot_API',
+        verbose_name = "Robot API",
+        description = 'Partie de l\'API dedans le robot',
+        categorie = 'communication',
+        ports = (
+            'socket_tcp',
+            'bus_4355',
+            'recv_position_data',
+            'send_position_data',
             )
         )
 robot = Component(
@@ -17,7 +32,7 @@ robot = Component(
         description = 'Partie de l\'API dedans le robot',
         categorie = 'robot_functions',
         ports = (
-            'socket_tcp',
+            'bus_4355',
             )
         )
 Qt_frame = Component(
@@ -27,19 +42,8 @@ Qt_frame = Component(
         categorie = 'frontend',
         ports = (
             'lidar_widget',
-            'movement_widget',
             'params_widget',
-            )
-        )
-django = Component(
-        name = "django",
-        verbose_name = "djangoiFrame",
-        description = "Carre developpe avec Python et Qt pour montrer l'information",
-        categorie = 'frontend',
-        ports = (
-            'lidar_widget',
-            'movement_widget',
-            'params_widget',
+            'mecanics_widget',
             )
         )
 ros = Component(
@@ -47,23 +51,50 @@ ros = Component(
         description = 'Program for processing and show lidar infor',
         categorie = 'data_process',
         ports = (
-            'recv_data',
-            'send_data',
+            'recv_lidar_data',
+            'send_lidar_data',
+            'recv_position_data',
+            'send_position_data',
             )
         )
 connections_list = [
         connect(# socket --> socket
-            port(communication_api,'socket_tcp'),
-            port(robot, 'sokcet_tcp'),
+            port(pc_api,'socket_tcp'),
+            port(robot_api, 'socket_tcp'),
+            bidirectional = True,
+            ),
+        connect(# bus inside robot
+            port(robot_api,'bus_4355'),
+            port(robot, 'bus_4355'),
+            bidirectional = True,
             ),
         connect(
-            port(ros, 'send_data'),
+            port(ros, 'send_lidar_data'),
             port(Qt_frame, 'lidar_widget'),
             ),
+        connect(# ros <-- qt frame
+            port(Qt_frame, 'lidar_widget'),
+            port(ros, 'recv_position_data'),
+            ),
+        connect(# api --> ros
+            port(pc_api, 'lidar_data_send'),
+            port(ros, 'recv_lidar_data'),
+            ),
         connect(
-            port(robot, 'socket_tcp'),
-            port(ros, 'recv_data'),
-            bidirectional = True,
-            message = "Hi",
+            port(Qt_frame, 'params_widget'),
+            port(pc_api, 'params'),
+            ),
+        connect(
+            port(pc_api,'robot_mecanics'),
+            port(Qt_frame, 'mecanics_widget'),
+            ),
+        connect(
+            port(ros,'send_position_data'),
+            port(pc_api, 'recv_position_data'),
+            ),
+        connect(
+            port(pc_api, 'send_position_data'),
+            port(robot_api, 'recv_position_data'),
             )
+
         ]
